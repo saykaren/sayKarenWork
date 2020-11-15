e744d741fc943b7a237fbeaca4639eee5179b3fd
Karen Garcia <saykaren@gmail.com>
2020-11-15 15:31:58 -0700

    Initial setup

---
+++ src/components/app/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styling/App.scss';
+// import Footer from './Footer';
+// import Container from './Container';
+
+const App = () => {
+  return (
+    <>
+      {/* <Container />
+      <Footer /> */}
+      Hello
+      <div className='App'>
+          Hello
+      </div>
+    </>
+  );
+};
+
+export default App;
