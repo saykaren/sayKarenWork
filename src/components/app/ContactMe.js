8096de60bf133467ead530cbd928456006916572
Karen Garcia <saykaren@gmail.com>
2020-11-24 21:02:32 -0700

    Updated Greyson Button Updated to have the link for Greyon on Website wording and updating styling to use the SCSS for website

--- src/components/app/ContactMe.js
+++ src/components/app/ContactMe.js
@@ -8,11 +8,11 @@ const ContactMe = () => (
     </section>
     <section id="email" className="contactMeDetails">
       <i className="fa fa-envelope" aria-hidden="true"></i>
-      <a href="mailto:saykaren@gmail.com">sayKaren@gmail.com</a>
+      <a href="mailto:saykaren@gmail.com" className="App-link">sayKaren@gmail.com</a>
     </section>
     <section id="phone" className="contactMeDetails">
       <i className="fa fa-phone" aria-hidden="true"></i>
-      <a href="tel:+3039468405">303.946.8405</a>
+      <a href="tel:+3039468405" className="App-link">303.946.8405</a>
     </section>
     <section id="address" className="contactMeDetails">
       <i className="fa fa-map-marker" aria-hidden="true"></i>
