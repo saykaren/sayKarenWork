8af9effb416f5ecdbbb212f5b906d423cb250818
Karen Garcia <saykaren@gmail.com>
2021-06-03 17:09:36 -0600

    Adds New Work and First Website

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import sayKarenLogo from '../assets/sayKarenTransparent.png';
+import developer from '../assets/developer_mode.png';
 
 const Footer = () => (
   <footer id="footer" className="footerDetails">
@@ -12,6 +13,14 @@ const Footer = () => (
         className="footerDetails"
         id="sayKarenLogo"
         alt="sayKaren.com"
+      />
+    </a>
+    <a href="http://saykaren.com" target="_blank" rel="noopener noreferrer">
+      <img
+        src={developer}
+        className="floatRight"
+        id="sayKarenLogo"
+        alt="developer mode"
       />
     </a>
   </footer>
